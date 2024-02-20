20b06174112d17fa5aca13a4b036abb302c01aca
ElRicki0 <ricardo06melara@gmail.com>
2024-02-20 10:16:42 -0600

    hola
    wenas

---
+++ controllers/utils/dinamics_parts.js
@@ -0,0 +1,11 @@
+const footer = document.querySelector("footer");
+
+footer.innerHTML = `<div class="social-icons-container ">
+<a href="https://twitter.com/?lang=es" class="social-icons"></a>
+<a href="https://www.instagram.com/" class="social-icons"></a>
+<a href="https://mail.google.com/" class="social-icons"></a>
+<a href="https://www.facebook.com/" class="social-icons"></a>
+</div>  
+<ul class="footer-menu-conteiner">
+</ul>
+<span class="copyright">&copy;2024, IVANE CARE. All rights reserved</span>`
