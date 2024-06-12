dc34495682fba86acf30823642fb159a250d5d29
AlissonZepeda07 <zepedaalisonivania@gmail.com>
2024-06-12 08:08:29 -0600

    carritofinal

--- api/models/handler/carrito_handler.php
+++ api/models/handler/carrito_handler.php
@@ -10,13 +10,13 @@ class CarritoHandler
      *   Declaración de atributos para el manejo de datos.
      */
 
-     protected $id_pedido = null;
-     protected $id_detalle = null;
-     protected $precio = null;
-     protected $cliente = null;
-     protected $producto = null;
-     protected $cantidad = null;
-     protected $estado = null;
+    protected $id_pedido = null;
+    protected $id_detalle = null;
+    protected $precio = null;
+    protected $cliente = null;
+    protected $producto = null;
+    protected $cantidad = null;
+    protected $estado = null;
     /*
      *   Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
      */
@@ -27,7 +27,7 @@ class CarritoHandler
     {
         $this->estado = 'Pendiente';
         $sql = 'SELECT id_pedido
-                FROM pedido
+                FROM tb_pedidos
                 WHERE estado_pedido = ? AND id_cliente = ?';
         $params = array($this->estado, $_SESSION['idCliente']);
         if ($data = Database::getRow($sql, $params)) {
@@ -43,9 +43,11 @@ class CarritoHandler
         if ($this->getOrder()) {
             return true;
         } else {
-            $sql = 'INSERT INTO pedido(direccion_pedido, id_cliente)
-                    VALUES((SELECT direccion_cliente FROM cliente WHERE id_cliente = ?), ?)';
-            $params = array($_SESSION['idCliente'], $_SESSION['idCliente']);
+            $this->estado = 'Pendiente';
+            // cambiar direccion_cliente por residencia_cliente según db
+            $sql = 'INSERT INTO tb_pedidos(direccion_pedido, id_cliente, estado_pedido, fecha_pedido)
+                    VALUES((SELECT direccion_cliente FROM tb_clientes WHERE id_cliente = ?), ?, ?, CURDATE())';
+            $params = array($_SESSION['idCliente'], $_SESSION['idCliente'], $this->estado);
             // Se obtiene el ultimo valor insertado de la llave primaria en la tabla pedido.
             if ($_SESSION['idPedido'] = Database::getLastRow($sql, $params)) {
                 return true;
@@ -55,47 +57,35 @@ class CarritoHandler
         }
     }
 
-     public function createRow()
-     {
-         // Insert the new client into tb_clientes
-         $sql = 'INSERT INTO tb_clientes(nombre_cliente, apellido_cliente, alias_cliente, correo_cliente, telefono_cliente, residencia_cliente, pass_cliente, estado_cliente, imagen_cliente)
+    public function createRow()
+    {
+        // Insert the new client into tb_clientes
+        $sql = 'INSERT INTO tb_clientes(nombre_cliente, apellido_cliente, alias_cliente, correo_cliente, telefono_cliente, residencia_cliente, pass_cliente, estado_cliente, imagen_cliente)
                  VALUES(?, ?, ?, ?, ?, ?, ?, true, ?)';
-         $params = array($this->nombre, $this->apellido, $this->alias, $this->correo, $this->telefono, $this->direccion, $this->clave, $this->imagen);
-         $clientCreated = Database::executeRow($sql, $params);
-     
-         if ($clientCreated) {
-             // Get the ID of the newly created client
-             $lastClientId = Database::getLastId();
-     
-             // Insert a new record in tb_pedidos with the client's ID
-             $sql = 'INSERT INTO tb_pedidos (id_cliente) VALUES(?)';
-             $params = array($lastClientId);
-             return Database::executeRow($sql, $params);
-         }
-         return false;
-     }
+        $params = array($this->nombre, $this->apellido, $this->alias, $this->correo, $this->telefono, $this->direccion, $this->clave, $this->imagen);
+        $clientCreated = Database::executeRow($sql, $params);
+
+        if ($clientCreated) {
+            // Get the ID of the newly created client
+            $lastClientId = Database::getLastId();
+
+            // Insert a new record in tb_pedidos with the client's ID
+            $sql = 'INSERT INTO tb_pedidos (id_cliente) VALUES(?)';
+            $params = array($lastClientId);
+            return Database::executeRow($sql, $params);
+        }
+        return false;
+    }
 
     public function readAll()
     {
-
         $sql = 'SELECT dp.id_detalle_entrega, dp.cantidad_pedido, dp.precio_pedido, ped.direccion_pedido AS direccion_pedido, ped.fecha_pedido AS fecha_pedido ,p.nombre_producto AS nombre_producto,c.nombre_cliente AS nombre_cliente
         FROM tb_detalles_pedidos dp
         INNER JOIN tb_pedidos ped ON dp.id_pedido = ped.id_pedido
         INNER JOIN tb_productos p ON dp.id_producto = p.id_producto
         INNER JOIN tb_clientes c ON ped.id_cliente = c.id_cliente
         ORDER BY dp.id_detalle_entrega';
-          return Database::getRows($sql);
-             $sql = 'SELECT dp.id_detalle_entrega, dp.cantidad_pedido, dp.precio_pedido, ped.direccion_pedido AS direccion_pedido, ped.fecha_pedido AS fecha_pedido ,p.nombre_producto AS nombre_producto,c.nombre_cliente AS nombre_cliente
-        FROM tb_detalles_pedidos dp
-         INNER JOIN tb_pedidos ped ON dp.id_pedido = ped.id_pedido
-         INNER JOIN tb_productos p ON dp.id_producto = p.id_producto
-         INNER JOIN tb_clientes c ON ped.id_cliente = c.id_cliente
-        ORDER BY dp.id_detalle_entrega';
-
-          return Database::getRows($sql);
-
-
-
+        return Database::getRows($sql);
     }
     public function readOnee()
     {
@@ -108,11 +98,12 @@ class CarritoHandler
 
     public function readlistcarrito()
     {
-        $sql = 'SELECT p.nombre_producto
-        FROM tb_pedidos pe
-        JOIN tb_detalles_pedidos dp ON pe.id_pedido = dp.id_pedido
-        JOIN tb_productos p ON dp.id_producto = p.id_producto
-        WHERE pe.id_pedido = ?';
+        $sql = 'SELECT p.nombre_producto, dp.cantidad_pedido, 
+        dp.cantidad_pedido * dp.precio_pedido as precio 
+                FROM tb_pedidos pe
+        JOIN tb_detalles_pedidos dp ON pe.id_pedido = dp.id_pedido 
+        JOIN tb_productos p ON dp.id_producto = p.id_producto 
+        WHERE pe.id_pedido =  ?';
         $params = array($this->id);
         return Database::getRows($sql, $params);
     }
@@ -138,8 +129,8 @@ class CarritoHandler
     public function createDetail()
     {
         // Se realiza una subconsulta para obtener el precio del producto.
-        $sql = 'INSERT INTO detalle_pedido(id_producto, precio_producto, cantidad_producto, id_pedido)
-                VALUES(?, (SELECT precio_producto FROM producto WHERE id_producto = ?), ?, ?)';
+        $sql = 'INSERT INTO tb_detalles_pedidos(id_producto, precio_pedido, cantidad_pedido, id_pedido)
+                VALUES(?, (SELECT precio_producto FROM tb_productos WHERE id_producto = ?), ?, ?)';
         $params = array($this->producto, $this->producto, $this->cantidad, $_SESSION['idPedido']);
         return Database::executeRow($sql, $params);
     }
@@ -147,11 +138,12 @@ class CarritoHandler
     // Método para obtener los productos que se encuentran en el carrito de compras.
     public function readDetail()
     {
-        $sql = 'SELECT id_detalle, nombre_producto, detalle_pedido.precio_producto, detalle_pedido.cantidad_producto
-                FROM detalle_pedido
-                INNER JOIN pedido USING(id_pedido)
-                INNER JOIN producto USING(id_producto)
-                WHERE id_pedido = ?';
+        $sql = 'SELECT dp.id_detalle_entrega, dp.precio_pedido,
+        p.nombre_producto, dp.cantidad_pedido, ROUND(dp.cantidad_pedido * dp.precio_pedido, 2) as precio 
+        FROM tb_pedidos pe 
+        JOIN tb_detalles_pedidos dp ON pe.id_pedido = dp.id_pedido 
+        JOIN tb_productos p ON dp.id_producto = p.id_producto 
+        WHERE pe.id_pedido = ?';
         $params = array($_SESSION['idPedido']);
         return Database::getRows($sql, $params);
     }
@@ -160,7 +152,7 @@ class CarritoHandler
     public function finishOrder()
     {
         $this->estado = 'Finalizado';
-        $sql = 'UPDATE pedido
+        $sql = 'UPDATE tb_pedidos
                 SET estado_pedido = ?
                 WHERE id_pedido = ?';
         $params = array($this->estado, $_SESSION['idPedido']);
@@ -170,14 +162,15 @@ class CarritoHandler
     // Método para actualizar la cantidad de un producto agregado al carrito de compras.
     public function updateDetail()
     {
-        $sql = 'UPDATE detalle_pedido
-                SET cantidad_producto = ?
-                WHERE id_detalle = ? AND id_pedido = ?';
-        $params = array($this->cantidad, $this->id_detalle, $_SESSION['idPedido']);
+        $sql = 'UPDATE tb_pedidos
+                SET estado_pedido = ?
+                WHERE id_pedido = ?';
+        $params = array('Cancelado', $_SESSION['idPedido']);
         return Database::executeRow($sql, $params);
     }
-    
+
     // Método para eliminar un producto que se encuentra en el carrito de compras.
+    // no se usa
     public function deleteDetail()
     {
         $sql = 'DELETE FROM detalle_pedido
--- controllers/public/carrito.js
+++ controllers/public/carrito.js
@@ -1,31 +1,52 @@
 // Constantes para completar las rutas de la API.
-const CARRITO_API = 'services/admin/carrito.php';
+const CARRITO_API = 'services/public/carrito.php';
 
 // Constantes para establecer el contenido de la tabla.
 const TABLE_BODY = document.getElementById('tarjetas');
-
-// Constantes para establecer el contenido de la tabla de productos.
-
 // Constante tipo objeto para obtener los parámetros disponibles en la URL.
 const PARAMS = new URLSearchParams(location.search);
+// constante con el obj del dom para cargar total a pagar
+const TOTAL = document.getElementById('total');
+// constante para agregar el cant de productos al dom
+const CANTIDAD = document.getElementById('cantidadproducto');
+// acciones para realizar (pagar y cancelar)
+const PAGAR = document.getElementById('pagar');
+const CANCELAR = document.getElementById('cancelar');
 
+//constante con el id del pedido
+const PEDIDO = PARAMS.get('idpedido')
 // Método del eventos para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', async () => {
 
     // Constante tipo objeto con los datos del producto seleccionado.
     const FORM = new FormData();
-    FORM.append('idProducto', PARAMS.get('id'));
+    FORM.append('idProducto', PEDIDO);
     // Petición para solicitar los datos del producto seleccionado.
-    const DATA = await fetchData(PRODUCTO_API, 'readOne', FORM);
+    const DATA = await fetchData(CARRITO_API, 'readDetail', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
-        // Se colocan los datos en la página web de acuerdo con el producto seleccionado previamente.
-        document.getElementById('imagenProducto').src = SERVER_URL.concat('images/productos/', DATA.dataset.imagen_producto);
-        document.getElementById('nombreProducto').textContent = DATA.dataset.nombre_producto;
-        document.getElementById('descripcionProducto').textContent = DATA.dataset.descripcion_producto;
-        document.getElementById('precioProducto').textContent = DATA.dataset.precio_producto;
-        document.getElementById('cantidadProducto').textContent = DATA.dataset.cantidad_producto;
-        document.getElementById('idProducto').value = DATA.dataset.id_producto;
+        // LIMPIAR ZONA DONDE CARGARAN LOS DATOS
+        TABLE_BODY.innerHTML = '';
+        let i = 1;
+        let total = [];
+        // agregando los datos de la db al DOM
+        DATA.dataset.forEach(element => {
+            TABLE_BODY.innerHTML += `
+                    <tr>
+                        <th scope="row">${i}</th>
+                        <td>${element.nombre_producto}</td>
+                        <td>${element.cantidad_pedido}</td>
+                        <td>$${element.precio_pedido}</td>
+                        <td>$${element.precio}</td>
+                    </tr>
+                `;
+            i++;
+            // guardando subtotales en un array para sumar, para sacar total
+            total[i] = element.precio;
+        });
+        // agregando al DOM la suma de los elementos del arreglo total
+        TOTAL.innerText = `$` + total.reduce((prev, current) => prev + current);
+        CANTIDAD.innerText = i + ' productos en carrito';
     } else {
         // Se presenta un mensaje de error cuando no existen datos para mostrar.
         document.getElementById('mainTitle').textContent = DATA.error;
@@ -34,85 +55,22 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
-// Método del evento para cuando el documento ha cargado.
-document.addEventListener('DOMContentLoaded', async () => {
-
-    const FORM = new FormData();
-    FORM.append('idProducto', PARAMS.get('id'));
-    // Petición para obtener las categorías disponibles.
-    const DATA = await fetchData(PRODUCTO_API, 'readOneMarca', FORM);
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (DATA.status) {
-
-        // Se establece la página web de destino con los parámetros
-        let url = `products.html?id=${row.id_producto}&nombre=${row.nombre_producto}`;
-        // Se crean y concatenan las tarjetas con los datos de cada categoría.
-        MARCAS.innerHTML = `
-                    <a href="marca_producto_especifico.html?id=${DATA.dataset.id_marca}&nombre=${DATA.dataset.nombre_marca}&imagen=${DATA.dataset.imagen_marca}&descrip=
-                    ${DATA.dataset.descripcion_marca}"class="btn btn-outline-primary">${DATA.dataset.nombre_marca}</a>
-`;
-
-    } else {
-        // Se asigna al título del contenido de la excepción cuando no existen datos para mostrar.
-        document.getElementById('mainTitle').textContent = DATA.error;
+// Método para finalizar el pedido a partir de la acción click del respectivo boton
+PAGAR.addEventListener('click', async () => {
+    confirm = await confirmAction('Desea finalizar la compra?');
+    if (confirm) {
+        const DATA = await fetchData(CARRITO_API, 'finishOrder');
+        (DATA.status) ? sweetAlert(1, DATA.message, false, 'index.html') : sweetAlert(2, DATA.error, false);
     }
 });
 
-// Método del evento para cuando el documento ha cargado.
-document.addEventListener('DOMContentLoaded', async () => {
-    
-    const FORM = new FormData();
-    FORM.append('id_producto', PARAMS.get('id'));
-    // Petición para obtener las categorías disponibles.
-    const DATA = await fetchData(PRODUCTO_API, 'read8Products');
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
-    if (DATA.status) {
-        // Se inicializa el contenedor de categorías.
-        SUGPRODUCTO.innerHTML = `
-        <div class="carousel-item active">
-        <div class="d-flex justify-content-center">
-            <div class="card">
-                <div class="img-wrapper">
-                    <img src="../../resources/img/imagenesIndex/producto8.jpg" alt="...">
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">Protector Solar Nivea Control De Brillo</h5>
-                    <p class="">$14.80
-                    </p>
-                    <div class="text-center mt-2">
-                        <a href="../public/descripcion_producto.html"
-                            class="btn btn_ver_mas">Más información</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>`;
-    
-        // Se recorre el conjunto de registros fila por fila a través del objeto row.
-        DATA.dataset.forEach(row => {
-
-            SUGPRODUCTO.innerHTML += `
-        <div class="carousel-item">
-        <div class="d-flex justify-content-center">
-            <div class="card">
-                <div class="img-wrapper">
-                    <img src="${SERVER_URL}images/productos/${row.imagen_producto}" alt="...">
-                </div>
-                <div class="card-body">
-                    <h5 class="card-title">${row.nombre_producto}</h5>
-                    <p class="">$${row.precio_producto}
-                    </p>
-                    <div class="text-center mt-2">
-                    <a href="producto.html?id=${row.id_producto}" class="btn">Ver detalle</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-`;
-        });
-    } else {
-        // Se asigna al título del contenido de la excepción cuando no existen datos para mostrar.
-        document.getElementById('mainTitle').textContent = DATA.error;
+// Método para finalizar el pedido a partir de la acción click del respectivo boton
+CANCELAR.addEventListener('click', async () => {
+    confirm = await confirmAction('Desea cancelar la compra?');
+    if (confirm) {
+        const FORM = new FormData();
+        FORM.append('idpedido', PEDIDO);
+        const DATA = await fetchData(CARRITO_API, 'deleteDetail', FORM);
+        DATA.status ? sweetAlert(1, DATA.message, false, 'index.html') : sweetAlert(2, DATA.error, false);
     }
-});
+})
