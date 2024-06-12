10149ed282cb5fd183ef56f06689701ecb2b0916
AlissonZepeda07 <zepedaalisonivania@gmail.com>
2024-06-12 09:06:45 -0600

    pull

--- api/services/public/carrito.php
+++ api/services/public/carrito.php
@@ -11,49 +11,51 @@ if (isset($_GET['action'])) {
     // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
     $result = array('status' => 0, 'session' => 0, 'message' => null, 'error' => null, 'exception' => null, 'dataset' => null);
     // Se verifica si existe una sesión iniciada como cliente para realizar las acciones correspondientes.
-    if (isset($_SESSION['id_cliente'])) {
+    if (isset($_SESSION['idCliente'])) {
         $result['session'] = 1;
         // Se compara la acción a realizar cuando un cliente ha iniciado sesión.
         switch ($_GET['action']) {
-            // Acción para agregar un producto al carrito de compras.
+                // Acción para agregar un producto al carrito de compras.
             case 'createDetail':
                 $_POST = Validator::validateForm($_POST);
                 if (!$pedido->startOrder()) {
                     $result['error'] = 'Ocurrió un problema al iniciar el pedido';
                 } elseif (
-                    !$pedido->setProducto($_POST['idProducto']) or
+                    !$pedido->setProductopedido($_POST['idProducto']) or
                     !$pedido->setCantidad($_POST['cantidadProducto'])
                 ) {
                     $result['error'] = $pedido->getDataError();
                 } elseif ($pedido->createDetail()) {
                     $result['status'] = 1;
                     $result['message'] = 'Producto agregado correctamente';
+                    $result['dataset'] =  $_SESSION['idPedido'];
                 } else {
                     $result['error'] = 'Ocurrió un problema al agregar el producto';
                 }
                 break;
-            // Acción para obtener los productos agregados en el carrito de compras.
+                // Acción para obtener los productos agregados en el carrito de compras.
             case 'readDetail':
                 if (!$pedido->getOrder()) {
                     $result['error'] = 'No ha agregado productos al carrito';
                 } elseif ($result['dataset'] = $pedido->readDetail()) {
                     $result['status'] = 1;
+                    // $result['dataset'] = $_S;
                 } else {
                     $result['error'] = 'No existen productos en el carrito';
                 }
                 break;
-            // Acción para remover un producto del carrito de compras.
+                // Acción para remover un producto del carrito de compras.
             case 'deleteDetail':
-                if (!$pedido->setIdDetalle($_POST['idDetalle'])) {
+                if (!$pedido->setCarritopedido($_POST['idpedido'])) {
                     $result['error'] = $pedido->getDataError();
-                } elseif ($pedido->deleteDetail()) {
+                } elseif ($pedido->updateDetail()) {
                     $result['status'] = 1;
                     $result['message'] = 'Producto removido correctamente';
                 } else {
                     $result['error'] = 'Ocurrió un problema al remover el producto';
                 }
                 break;
-            // Acción para finalizar el carrito de compras.
+                // Acción para finalizar el carrito de compras.
             case 'finishOrder':
                 if ($pedido->finishOrder()) {
                     $result['status'] = 1;
