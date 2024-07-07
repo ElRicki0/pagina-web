cc90d357879a99df6c73cc28bd9fb202965cdc2a
JLea2603 <20220638@ricaldone.edu.sv>
2024-07-07 12:43:54 -0600

    Actualizacion HomeScreen(Inicios de productos)

--- ivane_care/src/screens/HomeScreen.js
+++ ivane_care/src/screens/HomeScreen.js
@@ -1,16 +1,56 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, ScrollView, TextInput, FlatList } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, Image, ScrollView, TextInput, FlatList, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import Productos from '../components/Data/ProductosMuestra';
-
-
 import Boton from '../components/Button/Boton'; // Se importa el componente de boton para poder usarlo 
 
 
+const ip = '192.168.1.15'; // Dirección IP del servidor 
 
 const HomeScreen = ({ logueado, setLogueado }) => {
 
   const ProductosLista = Productos;
+
+  // Funcion para mostrar productos segun la base
+  const [ProductosL, setProductosL] = useState([]);
+
+  useEffect(() => {
+    getProductos();
+  }, []);
+
+
+  const getProductos = async () => {
+    try {
+      const response = await fetch(`http://${ip}/pagina-web/api/services/public/producto.php?action=readAll`, {
+        method: 'GET',
+      });
+
+      const data = await response.json();
+      if (data.status) {
+        setProductosL(data.dataset);
+      } else {
+        console.error(data.error);
+      }
+    } catch (error) {
+      console.error('Error al obtener los productos :', error);
+    }
+  };
+
+
+  // constante para renderizar los item de los productos
+  const renderProductCard = ({ item }) => {
+    return (
+      <TouchableOpacity style={styles.card}>
+        <Text style={styles.cardText}><Text style={styles.boldText}>imagen:</Text> {item.imagen_producto}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>Nombre:</Text> {item.nombre_producto}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>descripcion:</Text> {item.descripcion_producto}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>Precio:</Text> {item.precio_producto}</Text>
+        <Text style={styles.cardText}><Text style={styles.boldText}>Cantidad:</Text> {item.cantidad_producto}</Text>
+      </TouchableOpacity>
+    );
+  };
+
+
 
   // Estado, para la barra de busqueda 
   const [searchQuery, setSearchQuery] = useState('');
@@ -70,6 +110,22 @@ const HomeScreen = ({ logueado, setLogueado }) => {
           keyExtractor={(item) => item.id}
         />
       </View>
+
+      <View style={{ flexDirection: 'row', alignItems: 'center', marginTop: 23, marginBottom: 23 }}>
+        <View style={{ flex: 1, height: 1, backgroundColor: '#6C5FFF' }} />
+        <View>
+          <Text style={{
+            width: 90, textAlign: 'center', fontWeight: 'bold', fontSize: 16, color: '#6C5FFF'
+          }}>Nuestros productos base</Text>
+        </View>
+        <View style={{ flex: 1, height: 1, backgroundColor: '#6C5FFF' }} />
+      </View>
+
+      <FlatList
+        data={ProductosL}
+        renderItem={renderProductCard}
+        keyExtractor={(item) => item.id_producto.toString()}
+      />
     </ScrollView>
 
   );
@@ -128,7 +184,6 @@ const styles = StyleSheet.create({
     marginLeft: 5,
   },
   // Estilo de cards
-
   flatListContainer: {
     height: 250, // Altura fija para evitar el crecimiento automático
     marginHorizontal: 5,
@@ -169,5 +224,36 @@ const styles = StyleSheet.create({
     fontSize: 14,
     textAlign: 'center',
     flexWrap: 'wrap', // Permite que el texto se ajuste a múltiples líneas si es necesario
+  },
+
+
+  // estilo de cards prueba base
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    marginBottom: 20,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#333',
+  },
+  cardText: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: '#555',
+  },
+  boldText: {
+    fontWeight: 'bold',
   },
 });
