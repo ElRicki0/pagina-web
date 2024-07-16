13b3b8f30457c3dc14d4e309a348eba849d10e41
JLea2603 <20220638@ricaldone.edu.sv>
2024-07-16 14:20:57 -0600

    Cambios en diseño de graficas 1.2

--- controllers/admin/producto.js
+++ controllers/admin/producto.js
@@ -279,40 +279,13 @@ const graficoPastelCategorias = async () => {
             porcentajes.push(row.porcentaje);
         });
         // Llamada a la función para generar y mostrar un gráfico de pastel. Se encuentra en el archivo components.js
-        pieGraph('chart2', categorias, porcentajes, 'Porcentaje de productos por categoría');
+        pieGraph('chart2', categorias, porcentajes, '');
     } else {
         document.getElementById('chart2').remove();
         console.log(DATA.error);
     }
 }
 
-
-/*
-*   Función asíncrona para mostrar un gráfico de barras con la cantidad de productos por categoría.
-*   Parámetros: ninguno.
-*   Retorno: ninguno.
-*/
-const graficoBarrasCategorias = async () => {
-    // Petición para obtener los datos del gráfico.
-    const DATA = await fetchData(PRODUCTO_API, 'cantidadProductosCategoria');
-    // Se comprueba si la respuesta es satisfactoria, de lo contrario se remueve la etiqueta canvas.
-    if (DATA.status) {
-        // Se declaran los arreglos para guardar los datos a graficar.
-        let categorias = [];
-        let cantidades = [];
-        // Se recorre el conjunto de registros fila por fila a través del objeto row.
-        DATA.dataset.forEach(row => {
-            // Se agregan los datos a los arreglos.
-            categorias.push(row.nombre_categoria);
-            cantidades.push(row.cantidad);
-        });
-        // Llamada a la función para generar y mostrar un gráfico de barras. Se encuentra en el archivo components.js
-        barGraph('chart1', categorias, cantidades, 'Cantidad de productos', 'Cantidad de productos por categoría');
-    } else {
-        document.getElementById('chart1').remove();
-        console.log(DATA.error);
-    }
-}
 
 
 /*
@@ -336,7 +309,7 @@ const graficoLinealProductosVendidos = async () => {
             cantidadesVendidas.push(row.total_vendido);
         });
         // Llamada a la función para generar y mostrar un gráfico de líneas. Se encuentra en el archivo components.js
-        barGraph('chart1', productos, cantidadesVendidas, 'Cantidad Vendida', 'Top 5 productos más vendidos');
+        barGraph('chart1', productos, cantidadesVendidas, 'Cantidad Vendida', '');
     } else {
         document.getElementById('chart1').remove();
         console.log(DATA.error);
