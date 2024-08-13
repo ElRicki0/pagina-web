58efd731c458f2a8f10e77f1b4ee57b3d6db2be5
AlissonZepeda07 <107508188+AlissonZepeda07@users.noreply.github.com>
2024-08-13 14:20:36 -0600

    categorias visibles

--- ivane_care/contexts/Network.js
+++ ivane_care/contexts/Network.js
@@ -3,4 +3,5 @@
 
 
 // export const SERVER = "http://192.168.137.1:80//pagina-web/api/";   // COLEGIO
-export const SERVER = "http://192.168.1.15:80//pagina-web/api/"; // Leandro
+//export const SERVER = "http://192.168.1.15:80//pagina-web/api/"; // Leandro
+export const SERVER = "http://192.168.56.1:80//pagina-web/api/"; // Leandro
