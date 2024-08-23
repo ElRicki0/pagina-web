59c9925d2cf2729405e8218e0f4f7e49537d511e
JLea2603 <20220638@ricaldone.edu.sv>
2024-08-23 00:20:38 -0600

    Revisando funcionamiento de la app

--- ivane_care/contexts/Network.js
+++ ivane_care/contexts/Network.js
@@ -3,8 +3,8 @@
 
 
 // export const SERVER = "http://192.168.137.1:80//pagina-web/api/";   // Colegio F
-//export const SERVER = "http://192.168.1.15:80//pagina-web/api/"; // Leandro
+export const SERVER = "http://192.168.1.15:80//pagina-web/api/"; // Leandro
 
 //export const SERVER = "http://192.168.137.1:80//pagina-web/api/"; // Colegio L
 
-export const SERVER = "http://10.10.0.168:80//pagina-web/api/"; // Colegio L
+//export const SERVER = "http://10.10.0.168:80//pagina-web/api/"; // Colegio L
